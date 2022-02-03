3e174944fcdb371cd98604c4b7350e43ae9d0c09
mavi2011 <e4.mavi@gmail.com>
2022-02-03 12:01:17 +0530

    ff

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function preload(){
 }
 
 function setup(){
-    canvas = createCanvas(500, 680);
+    canvas = createCanvas(400, 400);
     canvas.center();
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status : Detecting Baby"; 
